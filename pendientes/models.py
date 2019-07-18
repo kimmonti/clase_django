67e722e24b8113ab15e71f2d040824b753cd634b
kimmonti <kim.montiel@gmail.com>
2019-07-18 18:27:25 -0400

    Welcome to programming front end

--- pendientes/models.py
+++ pendientes/models.py
@@ -6,3 +6,9 @@ class Tarea(models.Model):
     titulo = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
     descripcion = models.TextField(null=True, blank=True) #Campo/columna titulo de tipo Texto, los argumentos blank y null son para que el campo sea opcional
     estado = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.estado == True:
+            return "Tarea: " + self.titulo + ". Estado: Hecho"
+        else:
+            return "Tarea: " + self.titulo + ". Estado: En espera"
--- pendientes/views.py
+++ pendientes/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from random import randint
+from .models import Tarea #Importamos la clase tarea de models
 
 # Create your views here.
 
 def index(request):
-    saludo = "Hola, Mundo! Esta es la raiz /"
-    return HttpResponse(saludo) #retornamos el saludo
+    listita = Tarea.objects.all() #Consultamos la BD y exportamos los registros como objetos de la clase tarea
+    persona = {
+        "nombre":"Kim",
+        "edad": 24,
+        "hobbies": ["Ver pelis","Dibujar","Nadar"],
+        "Tareas": listita, #Completamos el diccionario con la lista en la clave Tareas para enviar a template
+        } 
+    return render(request, 'inicio.html', persona) #retornamos el saludo
+
+def pestañas(request):
+    saludo = "Hola, Mundo! Esta es la pestaña /"
+    return HttpResponse(saludo)
+
+def info(request):
+    informacion = "<h1>Penguin Bootcamp</h1><br><p>Esta página fue creada en el Penguin Bootcamp 4. Paraguay, 2019.</p>"
+    return HttpResponse(informacion)
+
+def aleatorio(request):
+    numero = str(randint(500,999))
+    return HttpResponse('El numero ganador es: ' + numero)
